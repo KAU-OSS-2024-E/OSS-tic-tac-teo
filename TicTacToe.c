31e3f4b2b9b0ed867e48fd2567b7b4adfb820dd0
james <jamesjang0622@naver.com>
2024-05-18 20:44:07 +0900

    add main_menu commit

--- TicTacToe.c
+++ TicTacToe.c
@@ -7,7 +7,9 @@ int board[10] = {2, 2, 2, 2, 2, 2, 2, 2, 2, 2}; //test1
 int turn = 1, flag = 0;
 int player, comp;
 
-void menu(); //menu //준영_commit 연습
+
+void main_menu(); //main_menu함수 추가
+void computer(); //기존 menu함수가 컴퓨터랑 게임하는 기능이여서 함수명을 computer()로 수정하였음
 void go(int n);
 void start_game();
 void check_draw();
@@ -25,42 +27,72 @@ void gotoxy(int x, int y)
 
 int main() //main function
 {
-  printf("hi");
-  system("cls");
-  menu();
+  system("cls"); // 화면을 클린 할 수 있도록 해줌
+  main_menu(); // 메인 메뉴 호출 
   getch();
-  printf("done");
+
   return 0;
 }
 
-void menu()
-{
-  int choice;
+void main_menu() // main_menu함수를 새로 만들었음
+{               //사용자가 첫 화면에 보았을 때 어떤 게임인지 알기 쉽고 컴튜터랑 게임을 할지 아니면 멀티플레이 게임을 할지 
+                //기능을 선택할 수 있게 해줌
+                //case 1에 있는 computer함수는
+                //
+                //case 2에 있는 멀티 플레이 함수 기능은 따로 만들어야 함
+  int choice; 
   system("cls");
-  printf("\n--------MENU--------");
-  printf("\n1 : Play with X");
-  printf("\n2 : Play with O");
-  printf("\n3 : Exit");
+  printf("\n-------- Tic Tac Toe Game--------");
+  printf("\n1. Play with computer ");
+  printf("\n2. Multiplay");
+
   printf("\nEnter your choice:>");
-  scanf("%d", &choice);
-  turn = 1;
-  switch (choice)
+  scanf("%d",&choice);
+
+  switch(choice)
   {
-  case 1:
-    player = 1;
-    comp = 0;
-    player_first();
-    break;
-  case 2:
-    player = 0;
-    comp = 1;
-    start_game();
-    break;
-  case 3:
-    exit(1);
-  default:
-    menu();
+    case 1:
+      computer();
+      break;
+    case 2:
+      //multiplay();
+      break;
+    case 3:  //프로그램 강제 종료
+      printf("End the program\n");
+      exit(1);
+    default:  // case 1,2,3에 해당되지 않는다면 재입력
+      main_menu();
   }
+}
+
+void computer() //컴퓨터랑 게임 
+{
+ int choice;
+ system("cls");
+ printf("\n--------loading--------");
+ printf("\n1 : Play with X"); // x가 먼저 시작함
+ printf("\n2 : Play with O"); // O로 진행
+ printf("\n3 : Exit");
+ printf("\nEnter your choice:>");
+ scanf("%d",&choice);
+ turn = 1;
+ switch (choice)
+ {
+  case 1:
+   player = 1;
+   comp = 0;
+   player_first();
+   break;
+  case 2:
+   player = 0;
+   comp = 1;
+   start_game();
+   break;
+  case 3:
+   exit(1);
+  default:
+   computer();
+ }
 }
 
 int make2()
