90c024916c3c74e54aa431c44be7b724cdeee3b0
Jang seungwoo <109260012+caffeineWoo@users.noreply.github.com>
2024-06-03 21:51:01 +0900

    Update game_rules.c
    BOARD_SIZE로 변경하였습니다.

--- src/game_rules.c
+++ src/game_rules.c
@@ -1,7 +1,7 @@
 #include "game_logic.h"
 #include "game_constants.h"
 
-int find_win_position(int p, int board[SIZE][SIZE]) // starting of function to check whether you put value at an empty space
+int find_win_position(int p, int board[BOARD_SIZE][BOARD_SIZE]) // starting of function to check whether you put value at an empty space
 {
   // p==1 then X   p==0  then  O
   int i, j;
@@ -13,9 +13,9 @@ int find_win_position(int p, int board[SIZE][SIZE]) // starting of function to c
     check_val = 50;
 
   // row check
-  for (i = 0; i < SIZE; i++) {
+  for (i = 0; i < BOARD_SIZE; i++) {
       if (board[i][0] * board[i][1] * board[i][2] == check_val) {
-          for (j = 0; j < SIZE; j++) {
+          for (j = 0; j < BOARD_SIZE; j++) {
               if (board[i][j] == EMPTY)
                   return i * 3 + j + 1;
           }
@@ -23,9 +23,9 @@ int find_win_position(int p, int board[SIZE][SIZE]) // starting of function to c
   }
 
   // column check
-  for (j = 0; j < SIZE; j++) {
+  for (j = 0; j < BOARD_SIZE; j++) {
       if (board[0][j] * board[1][j] * board[2][j] == check_val) {
-          for (i = 0; i < SIZE; i++) {
+          for (i = 0; i < BOARD_SIZE; i++) {
               if (board[i][j] == EMPTY)
                   return i * 3 + j + 1;
           }
@@ -34,14 +34,14 @@ int find_win_position(int p, int board[SIZE][SIZE]) // starting of function to c
 
 // diagonal check
   if (board[0][0] * board[1][1] * board[2][2] == check_val) {
-      for (i = 0; i < SIZE; i++) {
+      for (i = 0; i < BOARD_SIZE; i++) {
           if (board[i][i] == EMPTY)
               return i * 3 + i + 1;
       }
   }
 
   if (board[0][2] * board[1][1] * board[2][0] == check_val) {
-      for (i = 0; i < SIZE; i++) {
+      for (i = 0; i < BOARD_SIZE; i++) {
           if (board[i][2 - i] == EMPTY)
               return i * 3 + 2 - i + 1;
       }
