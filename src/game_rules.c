3b95b7142d9ee242fa9f01cf2713bd3e42367867
answjddn0607 <galaxy06079@gmail.com>
2024-06-04 22:10:39 +0900

    add comments at (game_constants.h, game_logic.c, game_rules.c)

--- src/game_rules.c
+++ src/game_rules.c
@@ -1,29 +1,25 @@
 #include "game_logic.h"
 #include "game_constants.h"
 
-int find_win_position(int p, int board[BOARD_SIZE][BOARD_SIZE]) // starting of function to check whether you put value at an empty space
-{
-  // p==1 then X   p==0  then  O
+int find_win_position(int p, int board[BOARD_SIZE][BOARD_SIZE]) {
   int i, j;
   int check_val;
 
-  if (p == 1)
-    check_val = 18;
+  if (p == 1) // p가 1이면 X, 0이면 O로 표시합니다.
+    check_val = 18; // 한 줄의 세 칸 중 두 칸이 채워져있고 한 칸이 비어져있으면 3*3*2 = 18 입니다.
   else
-    check_val = 50;
+    check_val = 50; // 한 줄의 세 칸 중 두 칸이 채워져있고 한 칸이 비어져있으면 5*5*2 = 50 입니다.
 
-  // row check
-  for (i = 0; i < BOARD_SIZE; i++) {
+  for (i = 0; i < BOARD_SIZE; i++) { // 각 행을 확인합니다.
       if (board[i][0] * board[i][1] * board[i][2] == check_val) {
           for (j = 0; j < BOARD_SIZE; j++) {
               if (board[i][j] == EMPTY)
-                  return i * 3 + j + 1;
+                  return i * 3 + j + 1; // 빈 공간의 위치를 반환합니다.
           }
       }
   }
 
-  // column check
-  for (j = 0; j < BOARD_SIZE; j++) {
+  for (j = 0; j < BOARD_SIZE; j++) { // 각 열을 확인합니다.
       if (board[0][j] * board[1][j] * board[2][j] == check_val) {
           for (i = 0; i < BOARD_SIZE; i++) {
               if (board[i][j] == EMPTY)
@@ -32,15 +28,14 @@ int find_win_position(int p, int board[BOARD_SIZE][BOARD_SIZE]) // starting of f
       }
   }
 
-// diagonal check
-  if (board[0][0] * board[1][1] * board[2][2] == check_val) {
+  if (board[0][0] * board[1][1] * board[2][2] == check_val) { // 대각선을 확인합니다. (좌상단 -> 우하단)
       for (i = 0; i < BOARD_SIZE; i++) {
           if (board[i][i] == EMPTY)
               return i * 3 + i + 1;
       }
   }
 
-  if (board[0][2] * board[1][1] * board[2][0] == check_val) {
+  if (board[0][2] * board[1][1] * board[2][0] == check_val) { // 대각선을 확인합니다. (우상단 -> 좌하단)
       for (i = 0; i < BOARD_SIZE; i++) {
           if (board[i][2 - i] == EMPTY)
               return i * 3 + 2 - i + 1;
